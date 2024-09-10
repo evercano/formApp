709ae4b06f820f4d4c86020872a6eaf394ee6c65
evercano <evercano.tech@gmail.com>
2024-09-10 17:19:13 -0600

    fist commit

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { RegisterPagesComponent } from './pages/register-pages/register-pages.component';
+
+const routes: Routes = [
+  {
+    path : '',
+    children : [
+      {path : 'sign-up' , component: RegisterPagesComponent },
+      {path : '**' , redirectTo : 'sign-up' },
+    ],
+  }   
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
